2afe585d0eb984dd57b058f877555d95f26dfa13
nfgrep <nfg2600@gmail.com>
2026-04-10 16:31:28 +0000

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/airhockey/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
