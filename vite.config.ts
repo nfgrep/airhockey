4fb7603cc6d4db94ad7401d00c742f1825855013
nfgrep <nfg2600@gmail.com>
2026-04-10 16:43:07 +0000

    Fix base path for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/airhockey/',
+  base: '/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
